6ac033ea672c43930e8fee86f73bcc4e5dcf0a87
Manasa-Reddy-M <manasa.intern@phonepe.com>
2022-06-28 13:38:32 +0530

    11 commit

--- copies/vm1_28.6.2022_13:28.sql
+++ copies/vm1_28.6.2022_13:28.sql
@@ -28,7 +28,9 @@ CREATE TABLE `SREPYMTS` (
   `email` varchar(40) DEFAULT NULL,
   `DOB` date DEFAULT NULL,
   `age` int(100) DEFAULT NULL,
-  `phn_num` int(11) DEFAULT NULL
+  `phn_num` int(11) DEFAULT NULL,
+  `username` varchar(40) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -41,4 +43,4 @@ CREATE TABLE `SREPYMTS` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-28 13:38:28
+-- Dump completed on 2022-06-28 13:28:00
